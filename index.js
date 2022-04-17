f1f1fd122948c59f1c565bc3e76ff5f82bc64b33
Todd Rylaarsdam <todd@toddr.org>
2022-04-16 23:07:01 -0500

    file structure init

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+app.get("/", (req, res) => {
+  res.send("Assignment Canvas v2 API");
+})
+
 app.listen(7001, () => {
   console.log("listening on port 7001");
 });
