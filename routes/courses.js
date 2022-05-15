b3fc758914a200f46c1d865984a51ebddfad483e
Todd Rylaarsdam <todd@toddr.org>
2022-05-15 15:10:17 -0500

    assignments for course

--- routes/courses.js
+++ routes/courses.js
@@ -40,6 +40,40 @@ router.get("/", async (req, res) => {
   }   
 })
 
+router.get("/assignments/:courseID", async (req, res) => {
+  try {
+    const canvasResults = await axios.get(`http://${dev.ip}/api/v1/courses/${req.params.courseID}/assignments?order_by=due_at`)
+
+    if(canvasResults.status === 200) {
+      res.send({
+        data: canvasResults.data,
+        status: "success"
+      })
+    }
+  } catch (error) {
+    console.log(error.response.status)
+    if(error.response.status === 401) {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+      res.send({
+        message: "api key unauthorized",
+        errorID: errorID,
+        status: "error"
+      })
+    }
+    else {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+
+      res.send({
+        message: `canvas api returned code ${error.response.status}`,
+        errorID: errorID,
+        status: "error"
+      })
+    }
+  }
+})
+
 router.get("/:courseID/users", async (req, res) => {
   try {
     const canvasResults = await axios.get(`http://${dev.ip}/api/v1/courses/${req.params.courseID}/users`)
