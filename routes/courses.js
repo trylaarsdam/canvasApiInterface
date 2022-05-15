7a77b7eeb0cddc5870987d4f7e152ebaf8886f81
Todd Rylaarsdam <todd@toddr.org>
2022-05-15 17:23:58 -0500

    updates

--- routes/courses.js
+++ routes/courses.js
@@ -74,10 +74,43 @@ router.get("/:id", async (req, res) => {
   }   
 })
 
-router.get("/assignments/:courseID", async (req, res) => {
+router.get("/:courseID/assignments", async (req, res) => {
   try {
-    const canvasResults = await axios.get(`http://${dev.ip}/api/v1/courses/${req.params.courseID}/assignments?order_by=due_at`)
+    const canvasResults = await axios.get(`http://${dev.ip}/api/v1/courses/${req.params.courseID}/assignments`)
 
+    if(canvasResults.status === 200) {
+      res.send({
+        data: canvasResults.data,
+        status: "success"
+      })
+    }
+  } catch (error) {
+    console.log(error.response.status)
+    if(error.response.status === 401) {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+      res.send({
+        message: "api key unauthorized",
+        errorID: errorID,
+        status: "error"
+      })
+    }
+    else {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+
+      res.send({
+        message: `canvas api returned code ${error.response.status}`,
+        errorID: errorID,
+        status: "error"
+      })
+    }
+  }
+})
+
+router.get("/:courseID/announcements", async (req, res) => {
+  try {
+    const canvasResults = await axios.get(`http://${dev.ip}/api/v1/announcements?context_codes[]=course_${req.params.courseID}&start_date=2018-01-01&end_date=2027-01-01`)
     if(canvasResults.status === 200) {
       res.send({
         data: canvasResults.data,
