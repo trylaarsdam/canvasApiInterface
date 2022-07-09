e6dc1d597b17e610e53a11cbfc94a0cdb85fa47c
Todd Rylaarsdam <todd@toddr.org>
2022-07-09 11:18:21 -0500

    get specific announcement

--- routes/announcements.js
+++ routes/announcements.js
@@ -97,4 +97,44 @@ router.get("/", async (req, res) => {
   }   
 })
 
+
+router.get("/:courseID/:announcementID", async (req, res) => {
+  try {
+    var canvasResults = await axios.get(`${req.user.canvasURL}/api/v1/courses/${req.params.courseID}/discussion_topics/${req.params.announcementID}`, {
+      headers: {
+        'Authorization': `Bearer ${req.user.canvasKey}`
+      }
+    })
+
+    if(canvasResults.status === 200) {
+      res.send({
+        data: canvasResults.data,
+        status: "success"
+      })
+    }
+  } catch (error) {
+    console.log("announcements")
+    console.log(error.response.status)
+    if(error.response.status === 401) {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+      res.send({
+        message: "api key unauthorized",
+        errorID: errorID,
+        status: "error"
+      })
+    }
+    else {
+      let errorID = uuidv4()
+      await log.logError(errorID, req.user.id, error)
+
+      res.send({
+        message: `canvas api returned code ${error.response.status}`,
+        errorID: errorID,
+        status: "error"
+      })
+    }
+  }   
+})
+
 module.exports = router;
