c1e0e46f900ffcdd9686d1d3fdfdf5a7b7140dee
Todd Rylaarsdam <todd@toddr.org>
2022-07-26 23:05:23 -0500

    include course in assignment

--- routes/assignments.js
+++ routes/assignments.js
@@ -22,6 +22,7 @@ router.get("/", async (req, res) => {
           }
         })
         for(var assignment of courseAssignments.data) {
+          assignment.course = course
           allAssignments.push(assignment)
         }
       } catch(error) {
