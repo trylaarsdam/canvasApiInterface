5c49a158738bf90c96a393f9ebfc35cdade3e254
Todd Rylaarsdam <todd@toddr.org>
2022-07-27 19:45:29 -0500

    updates

--- routes/announcements.js
+++ routes/announcements.js
@@ -148,7 +148,7 @@ router.get("/:courseID/:announcementID", async (req, res) => {
 
     if(mainPost.status === 200 && replies.status === 200) {
       mainPost.data.course = course
-      mainPost.data.replies = replies
+      mainPost.data.replies = replies.data
       console.log(mainPost.data)
       res.send({
         data: mainPost.data,
